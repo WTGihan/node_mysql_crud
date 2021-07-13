27a6c1e813beefa91e6cc1cef28c09c8be883def
WTGihan <wtgihan@gmail.com>
2021-07-13 12:35:05 +0530

    Sequlize crud complete

--- node_mysql_crud_sequlize/controllers/tutorial.controller.js
+++ node_mysql_crud_sequlize/controllers/tutorial.controller.js
@@ -14,7 +14,7 @@ exports.create = (req, res) => {
   const tutorial = {
     title: req.body.title,
     description: req.body.description,
-    published: req.body.published ? req.body.published : 0,
+    published: req.body.published ? req.body.published : false,
   };
 
   //   Save Tutorial in the database
@@ -26,6 +26,115 @@ exports.create = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occured while creating the Tutorial",
+      });
+    });
+};
+
+// retrieve the data
+
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}` } } : null;
+
+  Tutorial.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occured while retrieving tutorials",
+      });
+    });
+};
+
+// retreive single object
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Tutorial.findByPk(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id = " + id,
+      });
+    });
+};
+
+// update the object
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was updated successfully",
+        });
+      } else {
+        res.send({ message: `Cannot update Tutorial with id=${id}` });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id = " + id,
+      });
+    });
+};
+
+// delete an object
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Tutorial was deleted successfully" });
+      } else {
+        res.send({
+          message: `Cannot delete Tutorial with id=${id}`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id = " + id,
+      });
+    });
+};
+
+// delete all objects
+exports.deleteAll = (req, res) => {
+  Tutorial.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} Tutorials were deleted successfully` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials",
+      });
+    });
+};
+
+// find all objects by condition
+exports.findAllPublished = (req, res) => {
+  Tutorial.findAll({ where: { published: true } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
       });
     });
 };
--- node_mysql_crud_sequlize/routes/tutorial.routes.js
+++ node_mysql_crud_sequlize/routes/tutorial.routes.js
@@ -6,5 +6,23 @@ module.exports = (app) => {
   //   Create a new Tutorial
   router.post("/", tutorials.create);
 
+  //   Retrieve all Tutorials
+  router.get("/", tutorials.findAll);
+
+  //   Retrieve a single Tutorial with id
+  router.get("/:id", tutorials.findOne);
+
+  //   update the tutorial
+  router.put("/:id", tutorials.update);
+
+  //   delete tutorial
+  router.delete("/:id", tutorials.delete);
+
+  //   delete all tutorials
+  router.delete("/", tutorials.deleteAll);
+
+  //   retrieve all published tutorials
+  router.get("/published", tutorials.findAllPublished);
+
   app.use("/api/tutorials", router);
 };
